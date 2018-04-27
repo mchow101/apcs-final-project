5e3f1c5bdfc3630cb2d50dbb343471507dfa49cb
SchmidtLord <38797273+SchmidtLord@users.noreply.github.com>
2018-04-27 14:18:58 -0700

    Add files via upload

---
+++ Angband/src/Runner.java
@@ -0,0 +1,14 @@
+public class Runner {
+	public static void main (String [] args) {
+		
+		
+	char x = 'x';
+	char y = 'x';
+		
+	System.out.println(x == y);
+	
+	//System.out.println(str.substring(7));
+		
+		
+	}
+}
