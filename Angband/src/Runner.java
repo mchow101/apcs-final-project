3678a46c7c824a3d818ee2a85b7db5fd15f63bcc
unknown <mintychocolate101@gmail.com>
2018-04-30 21:36:57 -0700

    Updated Runner.java

--- Angband/src/Runner.java
+++ Angband/src/Runner.java
@@ -1,14 +1,5 @@
 public class Runner {
 	public static void main (String [] args) {
-		
-		
-	char x = 'x';
-	char y = 'x';
-		
-	System.out.println(x == y);
-	
-	//System.out.println(str.substring(7));
-		
-		
+	Graphics graphics = new Graphics();	
 	}
 }
